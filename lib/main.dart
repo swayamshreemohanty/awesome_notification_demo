61422077eabda0165b2acefd1d1356d46a47b149
Swayamshree Mohanty <swayamshree.appdev@colourmoon.com>
2023-09-22 19:00:58 +0530

    1st commit

---
+++ lib/main.dart
@@ -0,0 +1,155 @@
+import 'package:awesome_notification/awesome_notification/controller/notification_controller.dart';
+import 'package:awesome_notification/awesome_notification/screen/notification_screen.dart';
+import 'package:awesome_notification/firebase_options.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // Always initialize Awesome Notifications
+  await NotificationController.initializeLocalNotifications(debug: true);
+  await NotificationController.initializeRemoteNotifications(debug: true);
+  runApp(const MyApp());
+}
+
+///  *********************************************
+///     MAIN WIDGET
+///  *********************************************
+///
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  // The navigator key is necessary to navigate using static methods
+  static final GlobalKey<NavigatorState> navigatorKey =
+      GlobalKey<NavigatorState>();
+
+  static Color mainColor = const Color(0xFF9D50DD);
+
+  @override
+  State<MyApp> createState() => _AppState();
+}
+
+class _AppState extends State<MyApp> {
+  // This widget is the root of your application.
+
+  static const String routeHome = '/', routeNotification = '/notification-page';
+
+  @override
+  void initState() {
+    NotificationController.startListeningNotificationEvents();
+    super.initState();
+  }
+
+  List<Route<dynamic>> onGenerateInitialRoutes(String initialRouteName) {
+    List<Route<dynamic>> pageStack = [];
+    pageStack.add(MaterialPageRoute(
+        builder: (_) =>
+            const MyHomePage(title: 'Awesome Notifications Example App')));
+    if (initialRouteName == routeNotification &&
+        NotificationController.initialAction != null) {
+      pageStack.add(MaterialPageRoute(
+          builder: (_) => NotificationPage(
+              receivedAction: NotificationController.initialAction!)));
+    }
+    return pageStack;
+  }
+
+  Route<dynamic>? onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case routeHome:
+        return MaterialPageRoute(
+            builder: (_) =>
+                const MyHomePage(title: 'Awesome Notifications Example App'));
+
+      case routeNotification:
+        ReceivedAction receivedAction = settings.arguments as ReceivedAction;
+        return MaterialPageRoute(
+            builder: (_) => NotificationPage(receivedAction: receivedAction));
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Awesome Notifications - Simple Example',
+      navigatorKey: MyApp.navigatorKey,
+      onGenerateInitialRoutes: onGenerateInitialRoutes,
+      onGenerateRoute: onGenerateRoute,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+    );
+  }
+}
+
+///  *********************************************
+///     HOME PAGE
+///  *********************************************
+///
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Push the buttons below to create new notifications',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(width: 20),
+            FloatingActionButton(
+              heroTag: '1',
+              onPressed: () => NotificationController.createNewNotification(),
+              tooltip: 'Create New notification',
+              child: const Icon(Icons.outgoing_mail),
+            ),
+            const SizedBox(width: 10),
+            FloatingActionButton(
+              heroTag: '2',
+              onPressed: () => NotificationController.scheduleNewNotification(),
+              tooltip: 'Schedule New notification',
+              child: const Icon(Icons.access_time_outlined),
+            ),
+            const SizedBox(width: 10),
+            FloatingActionButton(
+              heroTag: '3',
+              onPressed: () => NotificationController.resetBadgeCounter(),
+              tooltip: 'Reset badge counter',
+              child: const Icon(Icons.exposure_zero),
+            ),
+            const SizedBox(width: 10),
+            FloatingActionButton(
+              heroTag: '4',
+              onPressed: () => NotificationController.cancelNotifications(),
+              tooltip: 'Cancel all notifications',
+              child: const Icon(Icons.delete_forever),
+            ),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
